1f1dcdb338ce93fd73edd8bba4ce660f817ae709
Simjaeryun <simjaeding@gmail.com>
2022-02-08 10:49:15 +0900

    gallery 생성

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,10 +1,23 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 export default function Gallery() {
-    const api = "f7cfb698e2ac45b786af0b554ec7cd09";
+    const api_key = "f7cfb698e2ac45b786af0b554ec7cd09";
+    const method1 = "flickr.interestingness.getList";
+    const num = 5;
+    const url = `https://www.flickr.com/services/rest/?method=${method1}&per_page=${num}&api_key=${api_key}&format=json&nojsoncallback=1`
+
     const main = useRef(null);
+
+    const [items, setItems] = useState([]);
     useEffect(() => {
         main.current.classList.add("on");
+        axios.get(url)
+            .then((data) => {
+                const photo = data.data.photos.photo
+                console.log(photo)
+                setItems(photo)
+            })
     }, [])
     return (
         <main className="content gallery" ref={main}>
@@ -12,7 +25,24 @@ export default function Gallery() {
             </figure>
             <div className="inner">
                 <h1>Gallery</h1>
-                <section></section>
+                <section>
+                    {items.map((data, idx) => {
+                        const imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`
+                        return (
+                            <article key={idx}>
+                                <div className="inner">
+                                    <div className="pic">
+                                        <img src={imgSrc} alt="flickr에서 가져온 이미지" />
+                                    </div>
+                                    <h2>
+                                        {data.title}
+                                    </h2>
+                                </div>
+
+                            </article>
+                        )
+                    })}
+                </section>
             </div>
         </main>
     )
