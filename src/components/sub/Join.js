ec4f6207947c27428a1171d2fe345efab29a5b3d
Simjaeryun <simjaeding@gmail.com>
2022-02-09 15:58:45 +0900

    form validation

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,17 +1,98 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Join() {
     const main = useRef(null);
+    const initVal = {
+        userid: '',
+    }
+    //변수 initVal 은 userid : '' 라는 key와 value값이있는 객체.
+    const [val, setVal] = useState(initVal);
+    // val 기본값으로 initVal을 가진다.
+    const [err, setErr] = useState({});
+    // err 기본값으로 빈 객체를 가진다.
+
+    //handleChange 실행 함수
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        //e.target = input이고 input에 name과 value를 배열에 담음
+        setVal({ ...val, [name]: value })
+        //useState 기존 값 val을 그대로 복사해주고 [name] : value를 담아준다
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //submit event를 막는다
+        setErr(check(val));
+        //err에 check(val)함수의 결과값을 담음
+    }
+
+    const check = val => {
+        let errs = {}
+        //errs 는 빈객체이다. 
+        if (!val.userid || val.userid.length < 5) {
+            //만약 userid가 존재하지않거나 5글자 보다 적다면
+            errs.userid = 'id를 5글자 이상 입력하세요';
+            //errs 빈객체에 string을 담는다
+        }
+        return errs;
+        //return을 이용해 errs를 내보낸다.
+    }
+
     useEffect(() => {
         main.current.classList.add("on");
     }, [])
+    //page가 시작할 때 처음 한번만 main에 on class를 추가한다.
+
+    useEffect(() => {
+        console.log(err);
+        //err의 값이 변경될 때 마다 콘솔에 err을 보여준다.
+
+    }, [err])
+
+
     return (
         <main className="content join" ref={main}>
             <figure>
             </figure>
             <div className="inner">
                 <h1>Join</h1>
-                <section></section>
+                <section>
+                    <form onSubmit={handleSubmit}>
+                        <fieldset>
+                            <legend>회원가입 폼 양식</legend>
+
+                            <table>
+                                <caption>회원가입 입력</caption>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">
+                                            <label htmlFor="userid">USER ID</label>
+                                        </th>
+                                        <td>
+                                            <input
+                                                type="text"
+                                                name="userid"
+                                                id="userid"
+                                                placeholder="아이디를 입력하세요"
+                                                value={val.userid}
+                                                onChange={handleChange}
+                                            />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th colSpan='2'>
+                                            <input type="reset" value='CANCLE' />
+                                            <input
+                                                type="submit"
+                                                value="SEND"
+                                            />
+                                        </th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </fieldset>
+                    </form>
+                </section>
             </div>
         </main>
     )
--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
+
 export default function Location() {
-    const main = useRef(null);
+    let main = useRef(null);
     const { kakao } = window;
-    const container = useRef(null)
+    const container = useRef(null);
     const [map, setMap] = useState(null);
     const [index, setIndex] = useState(0);
     const path = process.env.PUBLIC_URL;
@@ -10,42 +11,42 @@ export default function Location() {
         {
             title: "본점",
             latlng: new kakao.maps.LatLng(37.50711796614849, 126.7564159502457),
-            imgSrc: `${path}/img/marker1.png`,
+            imgSrc: path + '/img/marker1.png',
             imgSize: new kakao.maps.Size(232, 99),
             imgPos: { offset: new kakao.maps.Point(116, 99) },
         },
         {
             title: "지점1",
             latlng: new kakao.maps.LatLng(33.450701, 126.570667),
-            imgSrc: `${path}/img/marker2.png`,
+            imgSrc: path + '/img/marker2.png',
             imgSize: new kakao.maps.Size(232, 99),
             imgPos: { offset: new kakao.maps.Point(116, 99) },
         },
         {
             title: "지점2",
             latlng: new kakao.maps.LatLng(37.557527, 126.9222836),
-            imgSrc: `${path}/img/marker3.png`,
+            imgSrc: path + '/img/marker3.png',
             imgSize: new kakao.maps.Size(232, 99),
             imgPos: { offset: new kakao.maps.Point(116, 99) },
         }
     ];
-
     const [mapInfo] = useState(info);
 
     useEffect(() => {
-        main.current.classList.add("on");
+        main.current.classList.add('on');
+    }, []);
+
+    useEffect(() => {
+        container.current.innerHTML = '';
 
         const options = {
             center: mapInfo[0].latlng,
             level: 3
         }
 
-        new kakao.maps.Map(container.current, options)
+        const map = new kakao.maps.Map(container.current, options);
+        setMap(map);
 
-        const map = new kakao.maps.Map(container.current, options)
-        setMap(map)
-
-        // 마커출력 인스턴스 생성
         new kakao.maps.Marker({
             map: map,
             position: mapInfo[index].latlng,
@@ -55,47 +56,43 @@ export default function Location() {
 
         map.setCenter(mapInfo[index].latlng);
 
-        const mapSet = () => {
-            map.setCenter(mapInfo[index].latlng);
-        }
-        window.addEventListener("resize", mapSet)
+        const mapSet = () => map.setCenter(mapInfo[index].latlng);
 
-        return () => window.removeEventListener(mapSet)
-    }, [index])
+        const mapType = new kakao.maps.MapTypeControl();
+        map.addControl(mapType, kakao.maps.ControlPosition.TOPRIGHT)
 
-    // index state값이 실행
+        map.setZoomable(false)
+
+
+        window.addEventListener('resize', mapSet);
+
+        return () => window.removeEventListener('resize', mapSet);
+    }, [index]);
+
     return (
         <main className="content location" ref={main}>
-            <figure>
-            </figure>
+            <figure></figure>
+
             <div className="inner">
                 <h1>Location</h1>
                 <section>
                     <div id="map" ref={container}></div>
 
-                    <div className="map_btns">
-                        <nav className="traffic_btn">
-                            <button onClick={() => {
-                                map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-                            }}>교통정보보기</button>
-                            <button onClick={() => {
-                                map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-                            }}>교통정보닫기</button>
-                        </nav>
-                        <nav className="info_btn">
-                            {mapInfo.map((data, idx) => {
-                                return (
-                                    <button key={idx} onClick={() => {
-                                        setIndex(idx)
-                                    }}>
-                                        {data.title}
-                                    </button>
-                                )
-                            })}
-                        </nav>
-                    </div>
+                    <nav className='traffic'>
+                        <button onClick={() => {
+                            map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+                        }}>교통정보 보기</button>
 
+                        <button onClick={() => {
+                            map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+                        }}>교통정보 끄기</button>
+                    </nav>
 
+                    <nav className="branch">
+                        {mapInfo.map((data, idx) => {
+                            return <button key={idx} onClick={() => setIndex(idx)}>{data.title}</button>
+                        })}
+                    </nav>
                 </section>
             </div>
         </main>
