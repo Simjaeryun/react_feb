bf173b6b76f88282e933cc1c1282e9f018966c51
Simjaeryun <simjaeding@gmail.com>
2022-02-11 16:16:59 +0900

    로컬스토리지의 데이터를 main page에 불러오기

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,11 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function News() {
+    const getLocalItems = () => {
+        let data = localStorage.getItem('posts');
+
+        if (data) return JSON.parse(data);
+        else return [];
+    }
+
+    const [posts] = useState(getLocalItems);
+
     return (
         <section className="info">
             <div className="inner">
                 <h1>Recent Post</h1>
+                <ul>
+                    {posts.map((post, idx) => {
 
+                        return (
+                            <li key={idx}>
+                                <h2>{post.title}</h2>
+                                <p>{post.content}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </section>
     )
