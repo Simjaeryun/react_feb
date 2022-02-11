6674c082615e24357c558d35bd72358ce9cca9c3
Simjaeryun <simjaeding@gmail.com>
2022-02-11 10:26:21 +0900

    글 추가 기본기능 적용

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -2,33 +2,81 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Community() {
     const main = useRef(null);
-    let [index, setIndex] = useState(0);
+    const input = useRef(null);
+    const textarea = useRef(null);
+    const showBox = useRef(null);
+
+    const [posts, setPosts] = useState([
+        {
+            title: 'hello',
+            content: 'Here comes description in detail'
+        },
+        {
+            title: 'hello2',
+            content: 'Here comes description in detail2'
+        },
+    ]);
+
+    const createPost = (e) => {
+        e.preventDefault();
+        setPosts([
+            ...posts,
+            {
+                title: input.current.value,
+                content: textarea.current.value
+            }
+        ])
+        input.current.value = "";
+        textarea.current.value = "";
+    }
+
     useEffect(() => {
         main.current.classList.add("on");
     }, [])
-    useEffect(() => {
-        console.log("index값 변경됨");
-    }, [index])
+
     return (
         <main className="content community" ref={main}>
             <figure>
             </figure>
             <div className="inner">
                 <h1>Community</h1>
+
                 <section>
-                    <button onClick={() => setIndex(--index)}>-</button>
-                    <button onClick={() => setIndex(++index)}>+</button>
-                    <h2>{index}</h2>
+                    <div className='inputBox'>
+                        <input
+                            type="text"
+                            placeholder='제목을 입력하세요'
+                            ref={input}
+                        /><br />
+                        <textarea
+                            cols="30"
+                            rows="10"
+                            placeholder='본문을 입력하세요'
+                            ref={textarea}
+                        >
+                        </textarea><br />
+
+                        <button onClick={(e) => {
+                            e.preventDefault();
+                            input.current.value = "";
+                            textarea.current.value = "";
+                        }}>cancel</button>
+                        <button onClick={createPost}>create</button>
+                    </div>
+
+                    <div className="showList" ref={showBox}>
+                        {posts.map((post, idx) => {
+                            return (
+                                <article key={idx}>
+                                    <h2>{post.title}</h2>
+                                    <p>{post.content}</p>
+                                </article>
+                            )
+                        })}
+                    </div>
                 </section>
-            </div>
+            </div >
         </main >
     )
 }
 
-
-/*
-    useEffect : 해당 컴포넌트의 생성, 상태값변경, 소멸이라는 생명 주기에 따라 특정 구문을 실행 할 수 있는 훅
-    -- useEffect는 첫번째 인수로 콜백함수 등록
-    -- useEffect는 두번째 인수로 의존성을 등록
-    -- useEffect는 두번째 인수로 빈 배열을 의존성으로 등록 : 해당 컴포넌트가  처음 생성될 때 한번만 호출 가능.
-    */
