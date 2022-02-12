4dd11e3d1f332ad772a8fda49804241321b69e09
Simjaeryun <simjaeding@gmail.com>
2022-02-12 17:41:10 +0900

    side 컨텐츠 추가 및 px 단위조절

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,12 +2,13 @@ import Header from '../common/Header';
 import Visual from './Visual';
 import Intro from './Intro';
 import News from './News';
-
+import Side from './Side';
 export default function Main() {
 
     return (
         <>
             <Header type={'main'} />
+            <Side />
             <Visual />
             <Intro />
             <News />
---
+++ src/components/main/Side.js
@@ -0,0 +1,29 @@
+export default function Side() {
+    return (
+        <aside className="side">
+            <h1 className="side_logo">
+                SEASON 05
+            </h1>
+            <ul className="side_icon_box">
+                <li className="side_icon">
+                    <a href="">1</a>
+                </li>
+                <li className="side_icon">
+                    <a href="">2</a>
+                </li>
+                <li className="side_icon">
+                    <a href="">3</a>
+                </li>
+                <li className="side_icon">
+                    <a href="">4</a>
+                </li>
+                <li className="side_icon">
+                    <a href="">5</a>
+                </li>
+                <li className="side_icon">
+                    <a href="">5</a>
+                </li>
+            </ul>
+        </aside>
+    )
+}
