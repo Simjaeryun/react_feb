37a54d2dfd47ffe59b851aaf0b28892426e207ed
Simjaeryun <simjaeding@gmail.com>
2022-02-12 18:10:57 +0900

    visual 섹션

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,6 +1,18 @@
 export default function Visual() {
+    const path = process.env.PUBLIC_URL;
     return (
         <figure className="visual">
+            <div className="inner">
+
+                <div className="img_container">
+                    <div className="img_box">
+                        Banner
+                    </div>
+                </div>
+                <div className="txt">
+                    <h2>hi</h2>
+                </div>
+            </div>
         </figure>
     )
 }
