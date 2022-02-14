ba6f3c0dfbeb59043e06aed0ab13f2006752534f
Simjaeryun <simjaeding@gmail.com>
2022-02-15 01:35:47 +0900

    join page 디자인 작업

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 
 export default function Join() {
+    const path = process.env.PUBLIC_URL;
     const main = useRef(null);
     const initVal = {
         userid: '',
         pwd1: '',
         pwd2: '',
         email: '',
-        comments: '',
-        interests: false,
+
         gender: false,
         edu: false,
     }
@@ -26,15 +26,7 @@ export default function Join() {
         const isCheck = e.target.checked;
         setVal({ ...val, [name]: isCheck });
     }
-    const handleCheck = e => {
-        let isChecked = false;
-        const { name } = e.target;
-        const inputs = e.target.parentElement.querySelectorAll('input');
-        inputs.forEach(el => {
-            if (el.checked) isChecked = true;
-        });
-        setVal({ ...val, [name]: isChecked });
-    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsSubmit(true);
@@ -71,14 +63,8 @@ export default function Join() {
         if (val.pwd1 !== val.pwd2 || val.pwd2.length === 0) {
             errs.pwd2 = "두개의 비밀번호를 동일하게 입력하세요"
         }
-        if (val.comments.length < 10) {
-            errs.comments = "남기는말을 10글자 이상 입력하세요"
-        }
         if (!val.gender) {
             errs.gender = '성별을 선택하세요'
-        }
-        if (!val.interests) {
-            errs.interests = '관심사를 하나 이상 선택하세요'
         }
         if (!val.edu) {
             errs.edu = '학력을 선택해주세요'
@@ -114,173 +100,111 @@ export default function Join() {
                     <form onSubmit={handleSubmit}>
                         <fieldset>
                             <legend className="h">회원가입 폼 양식</legend>
-                            <table>
-                                <caption className="h">회원가입 입력</caption>
-                                <tbody>
-                                    {/* User ID */}
-                                    <tr>
-                                        <th scope="row">
-                                            <label htmlFor="userid">USER ID</label>
-                                        </th>
-                                        <td>
-                                            <input
-                                                type="text"
-                                                name="userid"
-                                                id="userid"
-                                                placeholder="아이디를 입력하세요"
-                                                value={val.userid}
-                                                onChange={handleChange}
-                                            />
-                                            <span className="err">{err.userid}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Password */}
-                                    <tr>
-                                        <th>
-                                            <label htmlFor="pwd1">password</label>
-                                        </th>
-                                        <td>
-                                            <input
-                                                type="password"
-                                                id="pwd1"
-                                                name="pwd1"
-                                                placeholder="비밀번호를 입력하세요"
-                                                value={val.pwd1}
-                                                onChange={handleChange}
-                                            />
-                                            <span className="err">{err.pwd1}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Re Password */}
-                                    <tr>
-                                        <th>
-                                            <label htmlFor="pwd2">Re Password</label>
-                                        </th>
-                                        <td>
-                                            <input
-                                                type="password"
-                                                id="pwd2"
-                                                name="pwd2"
-                                                placeholder="비밀번호를 입력하세요"
-                                                value={val.pwd2}
-                                                onChange={handleChange}
-                                            />
-                                            <span className="err">{err.pwd2}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Email */}
-                                    <tr>
-                                        <th>
-                                            <label htmlFor="email">E-mail</label>
-                                        </th>
-                                        <td>
-                                            <input
-                                                type="text"
-                                                id="email"
-                                                name="email"
-                                                placeholder="이메일 주소를 입력하세요"
-                                                value={val.email}
-                                                onChange={handleChange}
-                                            />
-                                            <span className="err">{err.email}</span>
-                                        </td>
-                                    </tr>
-                                    {/* edu */}
-                                    <tr>
-                                        <th scope="row">
-                                            <label htmlFor="edu">Education</label>
-                                        </th>
-                                        <td>
-                                            <select name="edu" id="edu" onChange={handleSelect}>
-                                                <option value="">학력을 선택하세요</option>
-                                                <option value="elementary-school">초등학교 졸업</option>
-                                                <option value="middle-school">중학교 졸업</option>
-                                                <option value="high-school">고등학교 졸업</option>
-                                                <option value="college">대학교 졸업</option>
-                                            </select>
-                                            <span className="err">{err.edu}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Gender */}
-                                    <tr>
-                                        <th scope="row">Gender</th>
-                                        <td>
-                                            <label htmlFor="male">Male</label>
-                                            <input
-                                                type="radio"
-                                                name="gender"
-                                                id="male"
-                                                onChange={handleRadio}
-                                            />
-                                            <label htmlFor="female">Female</label>
-                                            <input
-                                                type="radio"
-                                                name="gender"
-                                                id="female"
-                                                onChange={handleRadio}
-                                            />
-                                            <span className="err">{err.gender}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Interests */}
-                                    <tr>
-                                        <th scope="row">INTERESTES</th>
-                                        <td>
-                                            <label htmlFor="sports">Sports</label>
-                                            <input
-                                                type="checkbox"
-                                                name="interests"
-                                                id="sports"
-                                                onChange={handleCheck}
-                                            />
-                                            <label htmlFor="music">music</label>
-                                            <input
-                                                type="checkbox"
-                                                name="interests"
-                                                id="music"
-                                                onChange={handleCheck}
-                                            />
-                                            <label htmlFor="game">game</label>
-                                            <input
-                                                type="checkbox"
-                                                name="interests"
-                                                id="game"
-                                                onChange={handleCheck}
-                                            />
-                                            <span className="err">{err.interests}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Comments */}
-                                    <tr>
-                                        <th>
-                                            <label htmlFor="comments">LEAVE COMMENTS</label>
-                                        </th>
-                                        <td>
-                                            <textarea
-                                                name="comments"
-                                                id="comments"
-                                                value={val.comments}
-                                                onChange={handleChange}
-                                                cols="30"
-                                                rows="10"
-                                            ></textarea>
-                                            <span className="err">{err.comments}</span>
-                                        </td>
-                                    </tr>
-                                    {/* Button */}
-                                    <tr>
-                                        <th colSpan="2" className="btnSet">
-                                            <input type="reset" value='CANCLE' onClick={handleReset} />
-                                            <input
-                                                type="submit"
-                                                value="SEND"
-                                            />
-                                        </th>
-                                    </tr>
-                                </tbody>
-                            </table>
+
+                            {/* User ID */}
+                            <div className="form_input">
+                                <input
+                                    type="text"
+                                    name="userid"
+                                    id="userid"
+                                    placeholder="user id"
+                                    value={val.userid}
+                                    onChange={handleChange}
+                                />
+                                <span className="err">{err.userid}</span>
+                            </div>
+                            {/* Password */}
+                            <div className="form_input">
+                                <input
+                                    type="password"
+                                    id="pwd1"
+                                    name="pwd1"
+                                    placeholder="Password"
+                                    value={val.pwd1}
+                                    onChange={handleChange}
+                                />
+                                <span className="err">{err.pwd1}</span>
+                            </div>
+                            {/* Re Password */}
+                            <div className="form_input">
+                                <input
+                                    type="password"
+                                    id="pwd2"
+                                    name="pwd2"
+                                    placeholder="Re Password"
+                                    value={val.pwd2}
+                                    onChange={handleChange}
+                                />
+                                <span className="err">{err.pwd2}</span>
+                            </div>
+                            {/* Email */}
+                            <div className="form_input">
+                                <input
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Email"
+                                    value={val.email}
+                                    onChange={handleChange}
+                                />
+                                <span className="err">{err.email}</span>
+                            </div>
+                            {/* edu */}
+                            <div className="form_input">
+                                <label htmlFor="edu">Education</label>
+                                <select name="edu" id="edu" onChange={handleSelect}>
+                                    <option value="">학력을 선택하세요</option>
+                                    <option value="elementary-school">초등학교 졸업</option>
+                                    <option value="middle-school">중학교 졸업</option>
+                                    <option value="high-school">고등학교 졸업</option>
+                                    <option value="college">대학교 졸업</option>
+                                </select>
+                                <span className="err">{err.edu}</span>
+                            </div>
+                            {/* Gender */}
+                            <div className="form_input">
+                                <h2>Gender</h2>
+                                <div className="gender_input">
+                                    <label htmlFor="male">Male</label>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        id="male"
+                                        onChange={handleRadio}
+                                    />
+                                    <label htmlFor="female">Female</label>
+                                    <input
+                                        type="radio"
+                                        name="gender"
+                                        id="female"
+                                        onChange={handleRadio}
+                                    />
+                                </div>
+
+
+                                <span className="err">{err.gender}</span>
+
+                            </div>
+
+                            {/* Button */}
+                            <div className="form_input">
+                                <div className="btnSet">
+                                    <input
+                                        type="reset"
+                                        value='CANCLE'
+                                        onClick={handleReset}
+                                    />
+                                    <input
+                                        type="submit"
+                                        value="SEND"
+                                    />
+                                </div>
+                            </div>
                         </fieldset>
                     </form>
+                    <article className="form_img">
+                        <img src={`${path}/img/security2.png`} alt="" />
+                    </article>
                 </section>
             </div>
         </main>
