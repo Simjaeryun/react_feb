97f588a86781112a502e892c667b69a60a9c9e12
Simjaeryun <simjaeding@gmail.com>
2022-02-14 12:26:26 +0900

    btns 삭제

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -6,63 +6,14 @@ import Side from './Side';
 import Highlight from './Highlight';
 import Videos from './Videos';
 import Info from './info';
-import Btns from './Btns';
 
-import Anime from "../../class/anime.js";
-import { useEffect, useRef, useState } from 'react';
+import { useRef, } from 'react';
 
 
 
 export default function Main() {
     const main = useRef(null);
-    const pos = useRef([]);
 
-    const [index, setIndex] = useState(0);
-
-    const getIndex = index => {
-        setIndex(index);
-    }
-
-    const getPos = () => {
-        const sections = main.current.querySelectorAll('section');
-        let arr = [];
-        for (const sec of sections) {
-            arr.push(sec.offsetTop);
-        }
-        pos.current = arr;
-    }
-
-    const activation = () => {
-        const base = 200;
-        let scroll = window.scrollY;
-        const btns = main.current.querySelectorAll("#btns li");
-
-        pos.current.map((pos, idx) => {
-            if (scroll >= pos) {
-                for (const btn of btns) btn.classList.remove("on");
-                btns[idx].classList.add("on");
-            }
-        })
-    }
-
-
-    useEffect(() => {
-        getPos();
-        window.addEventListener("resize", getPos);
-        window.addEventListener("scroll", activation)
-        return () => {
-            window.removeEventListener("resize", getPos)
-            window.removeEventListener("scroll", activation)
-        }
-    }, [])
-
-    useEffect(() => {
-        new Anime(window, {
-            prop: 'scroll',
-            value: pos.current[index],
-            duration: 500
-        })
-    }, [index])
 
     return (
         <div id="mainWrap" ref={main}>
@@ -74,7 +25,6 @@ export default function Main() {
             <Intro />
             <News />
             <Info />
-            <Btns getIndex={getIndex} index={index} />
         </div>
     )
 }
