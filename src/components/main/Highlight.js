88ba628966a906cc9bc06ebe93c6c88696dd9bcd
Simjaeryun <simjaeding@gmail.com>
2022-02-20 20:32:38 +0900

    인덱스페이지 section highlight 이미지 flicker 데이터와 연동

--- src/components/main/Highlight.js
+++ src/components/main/Highlight.js
@@ -1,8 +1,11 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { useSelector } from 'react-redux';
 
 export default function Highlight() {
-
+    const path = process.env.PUBLIC_URL
+    const flickerImg = useSelector(state => state.flickerReducer.flicker)
+    console.log(flickerImg)
     return (
         <section className="highlight myScroll">
             <div className="inner">
@@ -11,60 +14,29 @@ export default function Highlight() {
                     <Swiper
                         className="img_container"
                         modules={[Navigation, Pagination, Scrollbar, A11y]}
-                        spaceBetween={50}
-                        slidesPerView={3}
+                        spaceBetween={15}
+                        slidesPerView={4}
                         pagination={{ clickable: true }}
                         scrollbar
                     >
+                        {flickerImg.map((data, idx) => {
+                            return (
+                                idx < 20 && idx > 12 ?
+                                    < SwiperSlide className="img_box" key={idx}>
+                                        <img src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_z.jpg`} alt="" />
+                                        <div className="img_txt">
+                                            <h2 className="img_title">
+                                                Lorem ipsum dolor sit.
+                                            </h2>
+                                            <p className="img_des">
+                                                Lorem ipsum, dolor sit amet consectetur adipisicing.
+                                            </p>
+                                        </div>
+                                    </SwiperSlide>
+                                    : null
+                            )
+                        })}
 
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
-                        <SwiperSlide className="img_box">
-                            <h2 className="img_title">
-                                Lorem ipsum dolor sit.
-                            </h2>
-                            <p className="img_txt">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing.
-                            </p>
-                        </SwiperSlide>
                     </Swiper>
                 </div>
             </div>
