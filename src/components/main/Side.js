6e9ea2fcb66e54c013ed29f701e7192c1e1cda18
Simjaeryun <simjaeding@gmail.com>
2022-02-28 14:26:07 +0900

    Side 링크연결 폰트어썸아이콘추가

--- src/components/main/Side.js
+++ src/components/main/Side.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faGoogle, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 export default function Side() {
     return (
         <aside className="side">
@@ -6,23 +10,52 @@ export default function Side() {
             </h1>
             <ul className="side_icon_box">
                 <li className="side_icon">
-                    <div>1</div>
+                    <a
+                        href='https://github.com/Simjaeryun'
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
                 </li>
                 <li className="side_icon">
-                    <div>2</div>
+                    <a
+                        href='https://www.facebook.com/'
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
                 </li>
                 <li className="side_icon">
-                    <div>3</div>
+                    <a
+                        href='https://www.instagram.com/'
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
                 </li>
                 <li className="side_icon">
-                    <div>4</div>
+                    <a
+                        href='https://www.twitter.com/'
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
                 </li>
+
                 <li className="side_icon">
-                    <div>5</div>
+                    <a
+                        href='https://www.google.com'
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faGoogle} />
+                    </a>
                 </li>
-                <li className="side_icon">
-                    <div>5</div>
-                </li>
+
             </ul>
         </aside>
     )
